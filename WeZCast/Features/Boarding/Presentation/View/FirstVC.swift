c85342af3297b1f195473a83058ad74dc0e7c1e4
Amin <mahmoudamin373@gmail.com>
2022-07-31 18:08:46 +0200

    first onboarding screen

---
+++ WeZCast/Features/Boarding/Presentation/View/FirstVC.swift
@@ -0,0 +1,17 @@
+//
+//  FirstVC.swift
+//  WeZCast
+//
+//  Created by Amin on 31/07/2022.
+//
+
+import UIKit
+
+class FirstVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+}
