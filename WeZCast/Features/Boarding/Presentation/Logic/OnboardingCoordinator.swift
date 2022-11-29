e1942ea1d6c7236163415898c1800f46c7e0c7e5
Amin <m.amin@sweater.sa>
2022-11-29 12:07:23 +0200

    home
    * navigating to home after finished onboarding

--- WeZCast/Features/Boarding/Presentation/Logic/OnboardingCoordinator.swift
+++ WeZCast/Features/Boarding/Presentation/Logic/OnboardingCoordinator.swift
@@ -10,17 +10,18 @@ import UIKit
 
 protocol OnboardingCoordinating:Coordinating{
     func navigateToNext()
+    func navigateToHome()
 }
 
 class OnboardingCoordinator : OnboardingCoordinating{
     
     private var nav:UINavigationController!
     private var usecase:OnboardingUsecaseProtocol!
-    private var userdefaults:LocalStorageInterface!
+//    private var userdefaults:LocalStorageInterface!
     
     init(nav:UINavigationController?){
         self.nav = nav
-        userdefaults = MyUserDefaults.shared
+        let userdefaults = MyUserDefaults.shared
         usecase = OnboardingUsecase(userdefaults: userdefaults)
     }
     func start() {
@@ -34,6 +35,10 @@ class OnboardingCoordinator : OnboardingCoordinating{
         let vc = OnboardingVC.init(nibName: R.nib.onboardingVC.name, bundle: nil)
         vc.onboarding = .second
         vc.viewModel = OnboardingViewModel(coordinator: self,usecase: usecase)
+        nav.pushViewController(vc, animated: true)
+    }
+    func navigateToHome() {
+        let vc = HomeVC.init(nibName: R.nib.homeVC.name, bundle: nil)
         nav.pushViewController(vc, animated: true)
     }
 }
